6cd833f9b5f07d2a24e49ea9c879f0f4a4a7b038
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-09-20 12:40:55 -0300

    Added subtraction as well as percentage calculation, which might be wrong... ALso added a method to check currency, it still crude.

--- app/models/Money.ts
+++ app/models/Money.ts
@@ -1,11 +1,34 @@
 export class Money {
   constructor(
-    private readonly centsAmount: number, 
+    private _centsAmount: number, 
     private readonly currency = 'BR'
   ) {}
+  
+  get amount() {
+    return this._centsAmount / 100
+  }
 
-  Add(value: Money): number {
-    let sum = (this.centsAmount + value.centsAmount) / 100
-    return sum;
+  percentage(percent: number): number {
+    return (percent / this._centsAmount) * 10000
+  }
+
+  add(value: number, currency?: string): number {
+    if (currency) {
+      this.validateCurrency(currency);
+    }
+    return (this._centsAmount + value) / 100;
+  }
+
+  subtract(value: number, currency?: string): number {
+    if (currency) {
+      this.validateCurrency(currency);
+    }
+    return (this._centsAmount - value) / 100;
+  }
+
+  private validateCurrency(currency: string) {
+    if (this.currency !== currency) {
+      throw new TypeError('It was providade a value in a different currency')
+    }
   }
 }

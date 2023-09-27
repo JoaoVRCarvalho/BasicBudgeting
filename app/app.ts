b2b44d9c40f84728ed243ddcadc347c5ef37e41a
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-09-27 12:37:11 -0300

    Added the controller for the money input

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,9 @@
+import { MoneyController } from "./controllers/money.controller.js";
+
 const form = document.querySelector('#amount-form')
+const controller = new MoneyController
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-
+  controller.add();
 })

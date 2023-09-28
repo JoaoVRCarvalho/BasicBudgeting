a612e05fd00d63eba31a142d5cf046fe9194bccf
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-09-28 12:56:42 -0300

    Implemented a toFixed adding decilmals without rounding

--- app/controllers/money.controller.ts
+++ app/controllers/money.controller.ts
@@ -10,10 +10,16 @@ export class MoneyController {
 
   add() {
     console.log(this._input.value);
+    // console.log(this._input.value);
   }
 
   private fixTwoDecimals(event: InputEvent) {
-    this._input.value = parseFloat(this._input.value).toFixed(2);
-    console.log("foi?")
+    const regExpression = new RegExp("^-?\\d+(?:\\.\\d{0," + 2 + "})?", "g");
+    const inputValue = this._input.value.match(regExpression)[0];
+    const dotIndex = inputValue.indexOf('.');
+    if (dotIndex === -1) return this._input.value =  `${inputValue}.${'0'.repeat(2)}`
+    const numberOfZeros= 2 - (inputValue.length - dotIndex);
+    return this._input.value =  numberOfZeros > 0 ? `${inputValue + '0'.repeat(numberOfZeros)}`: inputValue
+    // this._input.value = parseFloat(this._input.value).toFixed(2);
   }
 }

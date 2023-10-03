e23c3d029b132ad628aaa1c6a7235fd522c004d2
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-10-03 09:41:36 -0300

    Changed argument type

--- app/models/Utils.ts
+++ app/models/Utils.ts
@@ -1,16 +1,19 @@
 export class Utils {
 
-  static fixTwoDecimals(value: string) {
+  static fixTwoDecimals(num: string | number) {
+    let value;
+    typeof num !== 'string' ? value = num.toString() : value = num; 
+    
     const regExpression = new RegExp("^-?\\d+(?:\\.\\d{0," + 2 + "})?", "g");
-    const inputValue = this._input.value.match(regExpression)[0];
+    const inputValue = value.match(regExpression)[0];
     const commaIndex = inputValue.indexOf('.');
 
     if (commaIndex === -1) {
-      return this._input.value =  `${inputValue}.${'0'.repeat(2)}`
+      return value =  `${inputValue}.${'0'.repeat(2)}`
     }
     
     const numberOfZeros= 2 - (inputValue.length - commaIndex);
-    return this._input.value =  numberOfZeros > 0 ? `${inputValue + '0'.repeat(numberOfZeros)}`: inputValue
+    return value =  numberOfZeros > 0 ? `${inputValue + '0'.repeat(numberOfZeros)}`: inputValue
   }
 
 }

d242216ec1e248b75124e8b02cd895bf085fe42a
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-10-05 09:06:46 -0300

    Added the expenses array to budget

--- app/models/Budget.ts
+++ app/models/Budget.ts
@@ -1,9 +1,11 @@
 import { Money } from "./Money.js";
+import { Expense } from "./Expense.js";
 
 export class Budget extends Money {
   private _needsAmount = parseFloat(this.percentage(50));
   private _wantsAmount = parseFloat(this.percentage(30));
   private _savingsAmount = parseFloat(this.percentage(20));
+  private _expenses: Array<Expense>;
 
   // getters
   get needs(): number {
@@ -15,5 +17,13 @@ export class Budget extends Money {
   get savings(): number {
     return this._savingsAmount;
   }
+  
+  get expenses(): ReadonlyArray<Expense> {
+    return this._expenses;
+  }
+
+  public addExpense(expense: Expense) {
+    this._expenses.push(expense);
+  } 
 
 }

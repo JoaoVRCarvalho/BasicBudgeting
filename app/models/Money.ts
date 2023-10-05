58b517720c0d2bfcf1ebe4a4d32a5a9e379df54c
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-10-04 23:13:57 -0300

    Rewrote the class

--- app/models/Money.ts
+++ app/models/Money.ts
@@ -1,50 +1,50 @@
 import { Utils } from "./Utils.js";
 
-export class Money {
-  private readonly currency = 'BR';
-  private readonly amountCents: number;
+export abstract class Money {
+  protected amountCents: number
+  protected readonly currency = 'BR';
 
-  constructor(amount: string) {
+  constructor (amount: string) {
     this.amountCents = this.convertStringToCent(amount);
   }
-  
+
   get amount() {
-    const value = Utils.fixTwoDecimals(this.amountCents * 100);
+    const value = Utils.showDecimal(this.amountCents * 100);
     return value;
   }
 
   percentage(percent: number): string {
     const value = this.amountCents * (percent / 100) * 100;
-    return Utils.fixTwoDecimals(value);
+    return Utils.showDecimal(value);
   }
 
   add(moneyObj: Money): string {
     this.validateCurrency(moneyObj.currency);
     const value = (this.amountCents + moneyObj.amountCents) * 100;
-    return Utils.fixTwoDecimals(value);
+    return Utils.showDecimal(value);
   }
 
   subtract(moneyObj: Money): string {
     this.validateCurrency(moneyObj.currency);
     const value = (this.amountCents - moneyObj.amountCents) * 100;
-    return Utils.fixTwoDecimals(value);
+    return Utils.showDecimal(value);
   }
 
   multiply(moneyObj: Money): string {
     this.validateCurrency(moneyObj.currency);
     const value = (this.amountCents * moneyObj.amountCents) * 100;
-    return Utils.fixTwoDecimals(value); 
+    return Utils.showDecimal(value); 
   }
 
+  protected convertStringToCent(fullAmount: string): number {
+    const [amount, cents] = fullAmount.split('.');
+    const value = parseInt(amount) / 100 + ((parseInt(cents) / 100) / 100)  
+    return value;
+  }
+  
   private validateCurrency(currency: string) {
     if (this.currency !== currency) {
       throw new TypeError('It was providade a value in a different currency');
     }
-  }
-
-  private convertStringToCent(fullAmount: string): number {
-    const [amount, cents] = fullAmount.split('.');
-    const value = parseInt(amount) / 100 + ((parseInt(cents) / 100) / 100)  
-    return value;
   }
 }

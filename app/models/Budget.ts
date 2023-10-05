ceb9454e39c234639ca7eda62d3d2d9c8d378b53
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-10-05 13:54:20 -0300

    Added function to subtract cost from the amount that is the same type as the expense

--- app/models/Budget.ts
+++ app/models/Budget.ts
@@ -2,20 +2,20 @@ import { Money } from "./Money.js";
 import { Expense } from "./Expense.js";
 
 export class Budget extends Money {
-  private _needsAmount = parseFloat(this.percentage(50));
-  private _wantsAmount = parseFloat(this.percentage(30));
-  private _savingsAmount = parseFloat(this.percentage(20));
+  private _needsAmount = new Money(this.percentage(50));
+  private _wantsAmount = new Money(this.percentage(30));
+  private _savingsAmount = new Money(this.percentage(20));
   private _expenses: Array<Expense>;
 
   // getters
-  get needs(): number {
-    return this._needsAmount;
+  get needs(): string {
+    return this._needsAmount.amount;
   }
-  get wants(): number {
-    return this._wantsAmount;
+  get wants(): string {
+    return this._wantsAmount.amount;
   }
-  get savings(): number {
-    return this._savingsAmount;
+  get savings(): string {
+    return this._savingsAmount.amount;
   }
   
   get expenses(): ReadonlyArray<Expense> {
@@ -23,7 +23,27 @@ export class Budget extends Money {
   }
 
   public addExpense(expense: Expense) {
+    this.subtractFromAmount(expense);
     this._expenses.push(expense);
   } 
 
+  private subtractFromAmount(expense: Expense) {
+    switch(expense.type) {
+      case 'needs': {
+        this._needsAmount = new Money(this._needsAmount.subtract(expense.cost));
+        console.log(this._needsAmount);
+        break;
+      }
+      case 'wants': {
+        this._wantsAmount = new Money(this._wantsAmount.subtract(expense.cost));
+        console.log(this._wantsAmount);
+        break;
+      }
+      case 'savings' : {
+        this._savingsAmount = new Money(this._savingsAmount.subtract(expense.cost));
+        console.log(this._savingsAmount)
+        break;
+      }
+    }
+  }
 }

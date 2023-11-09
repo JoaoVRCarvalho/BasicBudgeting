31c4e4d1d08dcf87f115cc37feba46093814c65d
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-11-08 21:32:58 -0300

    Created abstract class for the view model

---
+++ app/views/view.ts
@@ -0,0 +1,16 @@
+export abstract class view<T> {
+  protected element: HTMLElement;
+
+  constructor(selector: string) {
+    if(!document.querySelector(selector)) {
+      throw `selector [${selector}] absent from DOM...`
+    }
+    this.element = document.querySelector(selector) as HTMLElement;
+  }
+
+  update(model: T) {
+    this.element.innerHTML = this.template(model);
+  }
+
+  abstract template(model: T): string
+}

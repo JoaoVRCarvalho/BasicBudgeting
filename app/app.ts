e6518f731b1e88dd90fca29effe29215e8dc1dfe
JoaoVRCarvalho <vitorrodriguesjc@gmail.com>
2023-11-13 22:15:11 -0300

    Improved front-end by adding some animations to the expense form

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,10 @@
 import { AmountController } from "./controllers/amount.controller.js";
 import { ExpenseController } from "./controllers/expense.controller.js";
 
-const amountForm = document.querySelector('#amount-form');
-const expenseForm = document.querySelector('#expense-form');
+const amountForm = document.querySelector('#amount-form') as HTMLElement;
+const expenseForm = document.querySelector('#expense-form') as HTMLElement;
+
+const showExpenseForm = document.querySelector('#show-expense-form');
 
 const amountController = new AmountController();
 const expenseController = new ExpenseController();
@@ -16,3 +18,23 @@ expenseForm.addEventListener('submit', event => {
   event.preventDefault();
   expenseController.add();
 })
+
+showExpenseForm.addEventListener('click', event => {
+  event.preventDefault();
+  let visibility = expenseForm.style.visibility;
+
+  if (visibility === '' || visibility === 'hidden') {
+    expenseForm.style.visibility = 'visible';
+    toggleExpenseFade();
+    setTimeout(toggleExpenseFade, 700);
+  } else {
+    expenseForm.style.visibility = 'hidden';
+  }
+})
+
+function toggleExpenseFade() {
+  if (expenseForm.classList.contains('fade')) {
+    return expenseForm.classList.remove('fade'); 
+  }
+  return expenseForm.classList.add('fade');
+}
